66d007236f22bcc2d74e86e32106478373980c63
zakaria-shahen <zakariya.shahen@gmail.com>
2023-08-26 21:47:45 +0300

    start

---
+++ src/main/java/com/example/mooc/MoocApplication.java
@@ -0,0 +1,13 @@
+package com.example.mooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mooc/MoocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
