2a6f65c2e996687f45d0157aa17d2d022dcf1f70
zakaria-shahen <zakariya.shahen@gmail.com>
2023-11-17 14:32:08 +0200

    add test container config

--- src/test/java/com/example/mooc/MoocApplicationTests.java
+++ src/test/java/com/example/mooc/MoocApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.mooc;
 
-import org.junit.jupiter.api.Test;
+import com.example.mooc.config.TestConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MoocApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.from(MoocApplication::main)
+				.with(TestConfig.class)
+				.run(args);
 	}
 
 }
---
+++ src/test/java/com/example/mooc/config/TestConfig.java
@@ -0,0 +1,19 @@
+package com.example.mooc.config;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.OracleContainer;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    @ServiceConnection
+    public OracleContainer oracleContainer() {
+        return new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+                .withReuse(true)
+                .withStartupTimeoutSeconds(300);
+    }
+
+}
