6699a3420fd36bce38943e8c87655c4c3e330343
Zakaria-Shahen <zakariya.shahen@gmail.com>
2024-01-17 11:55:47 +0200

    support Unpaged in Paging JdbcClientSqlInterceptorWith

--- src/main/java/com/example/mooc/repository/impl/interceptors/Paging.java
+++ src/main/java/com/example/mooc/repository/impl/interceptors/Paging.java
@@ -7,6 +7,9 @@ public class Paging implements JdbcClientSqlInterceptorWith<Pageable> {
 
     @Override
     public String intercept(String sql, Pageable pageable) {
+        if (pageable.isUnpaged()) {
+            return sql;
+        }
         long offset = pageable.getOffset(); // to make sure it's long or int always, to prevent SQL injection
         int pageSize = pageable.getPageSize(); // to make sure it's long or int always, to prevent SQL injection
         return STR."\{sql} offset \{offset} rows fetch next \{pageSize} rows only";
